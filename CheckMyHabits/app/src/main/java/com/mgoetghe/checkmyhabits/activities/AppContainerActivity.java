fca86a4ca061ecf14f3825cbd1b0d8a6e660dc80
Max <mgoetghe@gmail.com>
2017-09-02 18:20:06 +0200

    HomePageFeature: Linking HomeFragment display to AppActivity

--- CheckMyHabits/app/src/main/java/com/mgoetghe/checkmyhabits/activities/AppContainerActivity.java
+++ CheckMyHabits/app/src/main/java/com/mgoetghe/checkmyhabits/activities/AppContainerActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 
 import com.mgoetghe.checkmyhabits.R;
+import com.mgoetghe.checkmyhabits.fragments.HomeFragment;
 
 /**
  * This activity is the container for our whole application.
@@ -13,10 +14,24 @@ import com.mgoetghe.checkmyhabits.R;
  */
 
 public class AppContainerActivity extends FragmentActivity {
+
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        this.setContentView(R.layout.app_container_activity);
+        setContentView(R.layout.app_container_activity);
+
+        this.displayHome();
+    }
+
+    private void displayHome() {
+        HomeFragment homeFragment = new HomeFragment();
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.fragment_container, homeFragment).commit();
+    }
+
+    private void displayHabitsDetails() {
+
     }
 
 }
